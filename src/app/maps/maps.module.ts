72760443143b8c2c3d41588e4d39cc2ed53d9e53
Leonel Alejandro Torres Pérez <leyotorres1501@gmail.com>
2024-09-25 11:02:24 -0600

    Mapa Listo

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapScreenComponent } from './screens/map-screen/map-screen.component';
+import { MapViewComponent } from './components/map-view/map-view.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
 
 
 
 @NgModule({
   declarations: [
-    MapScreenComponent
+    MapScreenComponent,
+    MapViewComponent,
+    LoadingComponent,
+    BtnMyLocationComponent
   ],
   imports: [
     CommonModule
